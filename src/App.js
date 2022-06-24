5288f8b21162bd730da87422b5c0aa786b2c068d
pranshu <pranshu@modd.io>
2022-06-24 23:21:04 +0530

    V0.1

---
+++ src/App.js
@@ -0,0 +1,10 @@
+// import logo from "./logo.svg";
+// import "./App.css";
+
+import SolveExpression from "./components/SolveExpression/SolveExpression";
+
+function App() {
+  return <SolveExpression />;
+}
+
+export default App;
